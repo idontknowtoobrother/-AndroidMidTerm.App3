895ae58d0fdf327eb276b14049ba373b6c93fd85
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-09-04 20:33:14 +0700

    prepare: oildata for future if we have to use api done: init oilArrs

--- app/src/main/java/com/hexademical/androidmidtermapp3/MainActivity.kt
+++ app/src/main/java/com/hexademical/androidmidtermapp3/MainActivity.kt
@@ -2,24 +2,38 @@ package com.hexademical.androidmidtermapp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    private val oilArrs: Array<Oil> = arrayOf(
-        Oil("เชลล์ ฟิวเซฟ แก๊สโซฮอล์ E20", 34.44),
-        Oil("เชลล์ ฟิวเซฟ แก๊สโซฮอล์ 91", 35.28),
-        Oil("เชลล์ ฟิวเซฟ แก๊สโซฮอล์ 95", 35.55),
-        Oil("เชลล์ วี-เพาเวอร์ แก๊สโซฮอล์ 95", 43.04),
-        Oil("เชลล์ ดีเซล B20", 34.94),
-        Oil("ชลล์ ฟิวเซฟ ดีเซล", 34.94),
-        Oil("เชลล์ ฟิวเซฟ ดีเซล B7", 34.94),
-        Oil("เชลล์ วี-เพาเวอร์ ดีเซล", 34.94),
-        Oil("เชลล์ วี-เพาเวอร์ ดีเซล B7", 45.66)
-    )
+    private lateinit var oilArrs: ArrayList<Oil>;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // because of if one day we do a real app we should get data from api and init to oilArrs
+        val oilData = arrayOf(
+            arrayOf("เชลล์ ฟิวเซฟ แก๊สโซฮอล์ E20", 35.28),
+            arrayOf("เชลล์ ฟิวเซฟ แก๊สโซฮอล์ 91", 35.28),
+            arrayOf("เชลล์ ฟิวเซฟ แก๊สโซฮอล์ 95", 35.55),
+            arrayOf("เชลล์ วี-เพาเวอร์ แก๊สโซฮอล์ 95", 43.04),
+            arrayOf("เชลล์ ดีเซล B20", 34.94),
+            arrayOf("ชลล์ ฟิวเซฟ ดีเซล", 34.94),
+            arrayOf("เชลล์ ฟิวเซฟ ดีเซล B7", 34.94),
+            arrayOf("เชลล์ วี-เพาเวอร์ ดีเซล", 34.94),
+            arrayOf("เชลล์ วี-เพาเวอร์ ดีเซล B7", 45.66),
+        );
+
+        // after get data from api
+        oilArrs = ArrayList(); // initial array list
+        for(oil in oilData){
+            val oilInit = Oil(oil[0].toString(), oil[1].toString().toDouble())
+            oilArrs.add(oilInit)
+            Log.d("InitOilData", "Name:${oilInit.getOilName()} / Price:${oilInit.getOilPrice()}")
+        }
+
+
     }
 
-    
+
 }
